e0859a12be32b34f9958eda3ec8a95dd79085b93
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2024-08-02 14:34:39 +0330

    image-slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -14,7 +15,10 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* Star Rating Component */}
-      <StarRating />
+      {/* <StarRating /> */}
+
+      {/* Image Slider Component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
     </div>
   );
 }
