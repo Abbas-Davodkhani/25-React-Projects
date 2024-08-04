eb806a665317b2f9935ce58abb26aa75312098f5
Abbas-Davodkhani <davodkhaniabbas2003@gmail.com>
2024-08-04 09:25:31 +0330

    load-more-button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-button';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 
@@ -18,7 +19,10 @@ function App() {
       {/* <StarRating /> */}
 
       {/* Image Slider Component */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} /> */}
+    
+      {/* Load More Products Component */}
+      <LoadMoreData />
     </div>
   );
 }
